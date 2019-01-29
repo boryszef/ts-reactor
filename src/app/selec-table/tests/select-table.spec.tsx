90aaf41a20bcf860270336865c207cba7bd31b0f
Borys Szefczyk <boryszef@gmail.com>
2019-01-29 10:18:58 +0100

    work on tests

--- src/app/selec-table/tests/select-table.spec.tsx
+++ src/app/selec-table/tests/select-table.spec.tsx
@@ -1,21 +1,21 @@
+import * as React from 'react';
+import {shallow} from 'enzyme';
 import {SelecTable} from '../components';
 
 
 describe ('SelecTable tests', () => {
 
-    var props = {
-        headers: ['id', 'header1', 'header2'],
-        data: [
-            {id: 1, header1: 'abc1', header2: 'xyz1'},
-            {id: 2, header1: 'abc2', header2: 'xyz2'}
-        ],
-    };
-    var selectTable = new SelecTable(props);
+    let headers = ['id', 'head1'];
+    let data = [
+        {'id': 1, 'head1': 'a'},
+        {'id': 2, 'head1': 'b'},
+    ];
+    const table = shallow(<SelecTable headers={headers} data={data} />);
 
     it('does not normalize correct frame', () => {
         let dimensions = {x1: 0, y1: 0, x2: 10, y2: 10};
-        let st = selectTable._dimensions;
-        selectTable._normalizeFrame();
+        let st = table._dimensions;
+        table._normalizeFrame();
         expect(st).toEqual(dimensions);
     });
 });
